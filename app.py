2e1c1bd92b43d2a92affd054337eff8994dd8421
Projectsmarti <151812630+Projectsmarti@users.noreply.github.com>
2025-01-11 13:16:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -598,4 +598,4 @@ if __name__ == '__main__':
     Config.TEMP_FOLDER.mkdir(parents=True, exist_ok=True)
 
     # Start the Flask application
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
